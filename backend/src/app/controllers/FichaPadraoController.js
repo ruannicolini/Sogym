2183fc80484e3060ffa988bdab02225a7b6803fb
ruannicolini <ruan_03@hotmail.com>
2020-07-10 19:34:07 -0300

    update ficha Padrao - retornando array errros

--- backend/src/app/controllers/FichaPadraoController.js
+++ backend/src/app/controllers/FichaPadraoController.js
@@ -53,141 +53,294 @@ class FichaPadraoController {
   }
 
   async store(req, res) {
+    const validacao = { error: '' };
+    validacao.error = [];
+
     const schema = Yup.object().shape({
-      descricao: Yup.string().required(),
-      modalidade_id: Yup.number().required(),
-      professor_id: Yup.number().defined(),
-      ficha: Yup.array().defined(),
+      descricao: Yup.string().required('Campo descricao requerido'),
+      modalidade_id: Yup.number().required('Campo modalidade_id requerido'),
+      professor_id: Yup.number().required('Campo professor_id requerido'),
+      ficha: Yup.array().required('Campo ficha requerido'),
     });
 
-    if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation Fails' });
-    }
-
-    const fpEncontrada = await FichaPadrao.findOne({
-      where: { descricao: req.body.descricao },
-    });
-
-    if (fpEncontrada) {
-      return res.status(400).json({
-        error: 'Já existe uma ficha padrão com a descrição informada.',
+    try {
+      const vb = await schema.validate(req.body, { abortEarly: false });
+    } catch (err) {
+      err.inner.forEach((e) => {
+        validacao.error.push({
+          name: e.path,
+          message: e.message,
+        });
       });
     }
 
-    const modalidade = await Modalidade.findOne({
-      where: { id: req.body.modalidade_id },
-    });
+    if (req.body.descricao) {
+      const fpEncontrada = await FichaPadrao.findOne({
+        where: { descricao: req.body.descricao },
+      });
 
-    if (!modalidade) {
-      return res.status(400).json({ error: 'Modalidade não localizada' });
+      if (fpEncontrada) {
+        validacao.error.push({
+          name: 'descricao',
+          message: 'Já existe uma ficha padrão com a descrição informada.',
+        });
+      }
     }
 
-    const professor = await Usuario.findOne({
-      where: { id: req.body.professor_id, perfil_id: process.env.PROFESSOR },
-    });
+    if (req.body.modalidade_id) {
+      const modalidade = await Modalidade.findOne({
+        where: { id: req.body.modalidade_id },
+      });
 
-    if (!professor) {
-      return res.status(400).json({ error: 'Professor não localizado' });
+      if (!modalidade) {
+        validacao.error.push({
+          name: 'modalidade_id',
+          message: 'Modalidade não localizada.',
+        });
+      }
+    }
+
+    if (req.body.professor_id) {
+      const professor = await Usuario.findOne({
+        where: { id: req.body.professor_id },
+      });
+
+      if (!professor) {
+        validacao.error.push({
+          name: 'professor_id',
+          message: 'Professor não localizado',
+        });
+      } else {
+        const perfisUsuario = await professor.getPerfis();
+
+        var isProf = false;
+        await perfisUsuario.forEach((v) => {
+          if (v.id == process.env.PROFESSOR) {
+            isProf = true;
+          }
+        });
+
+        if (!isProf) {
+          validacao.error.push({
+            name: 'param /:id',
+            message: 'Usuário localizado não é um professor!',
+          });
+        }
+      }
+    }
+
+    if (req.body.ficha) {
+      for (const item of req.body.ficha) {
+        if (item.treino_id) {
+          const treino = await Treino.findByPk(item.treino_id);
+          if (!treino) {
+            validacao.error.push({
+              name: 'treino_id',
+              message: 'Treino COD ' + item.treino_id + 'não localizado',
+            });
+          }
+        } else {
+          validacao.error.push({
+            name: 'treino_id',
+            message: 'Campo treino_id não informado em item do array ficha',
+          });
+        }
+
+        if (item.exercicios) {
+          for (const exerc of item.exercicios) {
+            if (exerc.exercicio_id) {
+              const exercicio = await Exercicio.findByPk(exerc.exercicio_id);
+              if (!exercicio) {
+                validacao.error.push({
+                  name: 'exercicio_id',
+                  message:
+                    'Exercicio COD ' + exerc.exercicio_id + ' não localizado',
+                });
+              }
+            } else {
+              validacao.error.push({
+                name: 'exercicio_id',
+                message:
+                  'Campo exercicio_id não informado dentro do array ficha->exercicios',
+              });
+            }
+          }
+        } else {
+          validacao.error.push({
+            name: 'exercicios',
+            message: 'array exercicios não informado em item do array ficha',
+          });
+        }
+      }
+    }
+
+    if (validacao.error.length > 0) {
+      return res.status(400).json(validacao);
     }
 
     const fichaCriada = await FichaPadrao.create(req.body);
 
     for (const item of req.body.ficha) {
-      const treino = await Treino.findByPk(item.treino_id);
-
-      if (!treino) {
-        return res.status(400).json({ error: 'Treino não localizado' });
-      }
-
       for (const exerc of item.exercicios) {
-        const exercicio = await Exercicio.findByPk(exerc.exercicio_id);
-
-        if (!exercicio) {
-          return res.status(400).json({
-            error: 'Exercicio COD ' + exerc.exercicio_id + ' não localizado',
-          });
-        }
-
         const fpe = {
           ficha_padrao_id: fichaCriada.id,
           treino_id: item.treino_id,
           exercicio_id: exerc.exercicio_id,
           obs_execucao: exerc.obs_execucao,
         };
-
         FichaPadraoExercicio.create(fpe);
       }
     }
 
-    return res.json({ descricao: fichaCriada.descricao });
+    return res.json({ id: fichaCriada.id, descricao: fichaCriada.descricao });
   }
 
   async update(req, res) {
+    const validacao = { error: '' };
+    validacao.error = [];
+
     const schema = Yup.object().shape({
-      descricao: Yup.string().required(),
-      modalidade_id: Yup.number().required(),
-      professor_id: Yup.number().defined(),
-      ficha: Yup.array().defined(),
+      descricao: Yup.string().required('Campo descricao requerido'),
+      modalidade_id: Yup.number().required('Campo modalidade_id requerido'),
+      professor_id: Yup.number().required('Campo professor_id requerido'),
+      ficha: Yup.array().required('Campo ficha requerido'),
     });
 
-    if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation Fails' });
+    try {
+      const vb = await schema.validate(req.body, { abortEarly: false });
+    } catch (err) {
+      err.inner.forEach((e) => {
+        validacao.error.push({
+          name: e.path,
+          message: e.message,
+        });
+      });
     }
 
     const fpEncontrada = await FichaPadrao.findByPk(req.params.id, {
       include: [{ model: Exercicio, as: 'exercicios' }],
     });
     if (!fpEncontrada) {
-      return res.status(400).json({ error: 'Ficha padrão não encontrada.' });
+      validacao.error.push({
+        name: 'param /:id',
+        message: 'Ficha padrão não encontrada',
+      });
     }
 
     const { descricao } = req.body;
-    if (!(descricao === fpEncontrada.descricao)) {
-      const fpExists = await FichaPadrao.findOne({
-        where: { descricao },
+
+    console.log(descricao);
+
+    if (descricao) {
+      console.log(fpEncontrada.descricao);
+      if (!(descricao === fpEncontrada.descricao)) {
+        const fpExists = await FichaPadrao.findOne({
+          where: { descricao },
+        });
+
+        if (fpExists) {
+          validacao.error.push({
+            name: 'descricao',
+            message: 'Já existe uma ficha padrão com a descrição informada.',
+          });
+        }
+      }
+    }
+
+    if (req.body.modalidade_id) {
+      const modalidade = await Modalidade.findOne({
+        where: { id: req.body.modalidade_id },
       });
 
-      if (fpExists) {
-        return res.status(400).json({
-          error: 'Já existe uma ficha padrão com a descrição informada.',
+      if (!modalidade) {
+        validacao.error.push({
+          name: 'modalidade_id',
+          message: 'Modalidade não localizada.',
         });
       }
     }
 
-    const modalidade = await Modalidade.findOne({
-      where: { id: req.body.modalidade_id },
-    });
+    if (req.body.professor_id) {
+      const professor = await Usuario.findOne({
+        where: { id: req.body.professor_id },
+      });
 
-    if (!modalidade) {
-      return res.status(400).json({ error: 'Modalidade não localizada' });
-    }
-
-    const professor = await Usuario.findOne({
-      where: { id: req.body.professor_id, perfil_id: process.env.PROFESSOR },
-    });
-
-    if (!professor) {
-      return res.status(400).json({ error: 'Professor não localizado' });
-    }
-
-    //Valida novos itens Exercício
-    const listaExercicio = [];
-    for (const item of req.body.ficha) {
-      const treino = await Treino.findByPk(item.treino_id);
-      if (!treino) {
-        return res.status(400).json({
-          error: 'Treino COD ' + item.treino_id + ' não localizado',
+      if (!professor) {
+        validacao.error.push({
+          name: 'professor_id',
+          message: 'Professor não localizado',
         });
-      }
+      } else {
+        const perfisUsuario = await professor.getPerfis();
 
-      for (const exerc of item.exercicios) {
-        const exercicio = await Exercicio.findByPk(exerc.exercicio_id);
-        if (!exercicio) {
-          return res.status(400).json({
-            error: 'Exercicio COD ' + exerc.exercicio_id + ' não localizado',
+        var isProf = false;
+        await perfisUsuario.forEach((v) => {
+          if (v.id == process.env.PROFESSOR) {
+            isProf = true;
+          }
+        });
+
+        if (!isProf) {
+          validacao.error.push({
+            name: 'param /:id',
+            message: 'Usuário localizado não é um professor!',
+          });
+        }
+      }
+    }
+
+    if (req.body.ficha) {
+      for (const item of req.body.ficha) {
+        if (item.treino_id) {
+          const treino = await Treino.findByPk(item.treino_id);
+          if (!treino) {
+            validacao.error.push({
+              name: 'treino_id',
+              message: 'Treino COD ' + item.treino_id + 'não localizado',
+            });
+          }
+        } else {
+          validacao.error.push({
+            name: 'treino_id',
+            message: 'Campo treino_id não informado em item do array ficha',
           });
         }
 
+        if (item.exercicios) {
+          for (const exerc of item.exercicios) {
+            if (exerc.exercicio_id) {
+              const exercicio = await Exercicio.findByPk(exerc.exercicio_id);
+              if (!exercicio) {
+                validacao.error.push({
+                  name: 'exercicio_id',
+                  message:
+                    'Exercicio COD ' + exerc.exercicio_id + ' não localizado',
+                });
+              }
+            } else {
+              validacao.error.push({
+                name: 'exercicio_id',
+                message:
+                  'Campo exercicio_id não informado dentro do array ficha->exercicios',
+              });
+            }
+          }
+        } else {
+          validacao.error.push({
+            name: 'exercicios',
+            message: 'array exercicios não informado em item do array ficha',
+          });
+        }
+      }
+    }
+
+    if (validacao.error.length > 0) {
+      return res.status(400).json(validacao);
+    }
+
+    const listaExercicio = [];
+    for (const item of req.body.ficha) {
+      for (const exerc of item.exercicios) {
         const fpe = {
           ficha_padrao_id: fpEncontrada.id,
           treino_id: item.treino_id,
@@ -213,7 +366,7 @@ class FichaPadraoController {
       return res.status(500).send();
     }
 
-    return res.json({ descricao: descricao });
+    return res.json({ id: fpEncontrada.id, descricao: descricao });
   }
 
   async delete(req, res) {
