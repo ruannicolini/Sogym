98ab9621c199e39045a8c87825e315d4110610f0
ruannicolini <ruan_03@hotmail.com>
2020-07-17 17:33:54 -0300

    cors add

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,13 @@ import 'dotenv/config';
 import path from 'path';
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(routes);
     // servir arquivos staticos
